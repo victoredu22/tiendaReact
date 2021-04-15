43adba29ba7e55b63a6e6a36688a2d3c5a07e76c
victor <victor.edu22@gmail.com>
2021-04-15 12:56:45 -0400

    save

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -8,12 +8,16 @@ import {
 import { NavBar } from "../components/index/NavBar";
 import { PieScreen } from "../components/index/PieScreen";
 import "animate.css";
+import { createBrowserHistory } from 'history';
 
 import { LayoutScreen } from "../components/layout/LayoutScreen";
 
+const history = createBrowserHistory({
+  basename: 'base-name'
+}) 
 export const AppRouter = () => {
 	return (
-		<Router history={history}>
+		<Router history={history} >
 			<NavBar />
 
 			<div>
